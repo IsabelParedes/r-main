16c3db6328ae7b7cd706bcc00b5fd8b8c57dbbe6
Isabel Paredes <isabel.paredes@quantstack.net>
2026-08-20 17:17:38 +0200

    Add env vars

--- src/rmain_pre.js
+++ src/rmain_pre.js
@@ -4,6 +4,8 @@ Module["preRun"] = [].concat(Module["preRun"] || [], () => {
     R_LIBS: "/lib/R/library",
     R_LIBS_USER: "NULL",
     R_LIBS_SITE: "NULL",
+    R_INSTALL_LIBRARY: "/lib/R/library",
+    R_ENVIRON: "/lib/R/etc/Renviron",
     LD_LIBRARY_PATH: "/lib/R/lib:/lib",
     R_ENABLE_JIT: "0",
   });
