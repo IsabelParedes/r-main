b2593b646d70da5ea2fd6c7b07b25a14013dc279
Isabel Paredes <isabel.paredes@quantstack.net>
2026-09-04 17:49:07 +0200

    Set timezone var

--- src/rmain_pre.js
+++ src/rmain_pre.js
@@ -1,13 +1,11 @@
 Module["preRun"] = [].concat(Module["preRun"] || [], () => {
   Object.assign(ENV, {
     R_HOME: "/lib/R",
-    R_LIBS: "/lib/R/library",
     R_LIBS_USER: "NULL",
     R_LIBS_SITE: "NULL",
-    R_INSTALL_LIBRARY: "/lib/R/library",
     R_ENVIRON: "/lib/R/etc/Renviron",
-    LD_LIBRARY_PATH: "/lib/R/lib:/lib",
     R_ENABLE_JIT: "0",
     PYTHONHOME: "/",
+    TZ: Intl.DateTimeFormat().resolvedOptions().timeZone || "UTC",
   });
 });
